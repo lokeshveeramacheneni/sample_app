f7673867bf14333fa7d3246653e7c1372ff71aba
Lokesh Veeramacheneni <lokeshveeramacheneni27@gmail.com>
2022-09-25 18:01:13 +0200

    Added help page as excersice

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/contact'
   get 'pages/about'
+  get 'pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
