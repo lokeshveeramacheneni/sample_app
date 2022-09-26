9d60bb1170ac0fce916f070b998121fe96476bb1
Lokesh Veeramacheneni <lokeshveeramacheneni27@gmail.com>
2022-09-26 22:04:36 +0200

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
-Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+##### OLD RAILS ROUTING CODE
+#Rails.application.routes.draw do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#  get 'pages/home'
+#  get 'pages/contact'
+#  get 'pages/about'
+#  get 'pages/help'
+#  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#
+#  # Defines the root path route ("/")
+#  # root "articles#index"
+#end
+
+##### NEW RAILS ROUTING CODE
+SampleApp::Application.routes.draw do
+  get 'users/new'
+  get '/contact', :to => 'pages#contact'
+  get '/about', :to => 'pages#about'
+  get '/help', :to => 'pages#help'
+  root :to => 'pages#home'
+
+  get '/signup', :to=> 'users#new'
 end
+
